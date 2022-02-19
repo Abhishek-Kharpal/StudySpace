4ded4c03829b4e17d4b95c692458b4d224c2e703
Abhishek-Kharpal <abhikharpal@gmail.com>
2022-02-19 21:55:08 +0530

    env example file added

--- StudySpace/StudySpace/settings.py
+++ StudySpace/StudySpace/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vp*lb(f$say$ca6d3!=7vz3opt@a4c6$#4h*^6#g^jvp0ny^*n'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
